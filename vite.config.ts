28ea8691a96d71cfc3132bb7ff468b88a40a5d76
ABL <zbyrd72@gmail.com>
2023-10-15 13:47:32 -0400

    changed env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const API_TARGET = process.env.VITE_API_TARGET || "http://127.0.0.1:5000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: API_TARGET,
         changeOrigin: true,
       },
       "/auth": {
-        target: "http://127.0.0.1:5000",
+        target: API_TARGET,
         changeOrigin: true,
       },
     },
