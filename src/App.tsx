fa4aa7061209defcbc752958197f013abdf47c1f
ABL <zbyrd72@gmail.com>
2023-10-18 17:25:55 -0400

    incorporated health check

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./assets/styles/global.css";
 import NavBar from "./components/Navbar/NavBar";
 import SignIn from "./components/SignIn/SignIn";
@@ -8,8 +10,31 @@ import AddBeer from "./pages/AddBeer/AddBeer";
 import BeerItem from "./pages/BeerItem/BeerItem";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./UserContext";
+import { API_BASE_URL } from "./config";
 
 function App() {
+  const [isLoadingBackend, setIsLoadingBackend] = useState(true);
+
+  useEffect(() => {
+    const checkBackendHealth = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/health`);
+        if (response.status === 200) {
+          setIsLoadingBackend(false);
+        }
+      } catch (error) {
+        console.error("Error checking backend health:", error);
+        setTimeout(checkBackendHealth, 5000); // Retry after 5 seconds if there's an error
+      }
+    };
+
+    checkBackendHealth();
+  }, []);
+
+  if (isLoadingBackend) {
+    return <div>Loading...</div>; // Render a loading screen while waiting for the backend
+  }
+
   return (
     <Router>
       <UserProvider>
